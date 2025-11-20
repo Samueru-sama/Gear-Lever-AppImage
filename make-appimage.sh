f6cc5f4551bc315bd10b77358ea9ce983487c99e
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-20 10:05:47 +0100

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,15 +11,16 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/icons/hicolor/scalable/apps/it.mijorus.gearlever.svg
 export DESKTOP=/usr/share/applications/it.mijorus.gearlever.desktop
 export DEPLOY_SYS_PYTHON=1
-export DEBLOAT_SYS_PYTHON=0
 export DEPLOY_OPENGL=1
 export DEPLOY_GTK=1
 export GTK_DIR=gtk-4.0
 export PATH_MAPPING='/usr/share/gearlever:${SHARUN_DIR}/share/gearlever'
+export DEPLOY_LOCALE=1
 export STARTUPWMCLASS=gearlever # For Wayland, this is 'it.mijorus.gearlever', so this needs to be changed in desktop file manually by the user in that case until some potential automatic fix exists for this
 
 # Deploy dependencies
 quick-sharun /usr/bin/gearlever \
+             /usr/lib/libgirepository* \
              /usr/bin/dwarfs \
              /usr/bin/mkdwarfs \
              /usr/bin/dwarfsextract \
